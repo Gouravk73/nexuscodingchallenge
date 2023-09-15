b93fb873c1b7ed710c2a05446426f5e63b4cb8f9
Gourav <Gouravkumar.1273@gmail.com>
2023-09-16 02:33:08 +0530

    skin-care

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Hero from './components/Hero';
+import Fragrance from './components/Fragrance';
+import SkinCare from './components/SkinCare';
 
 function App() {
   return (
     <BrowserRouter>
+      <div className='bg-orange-50'>
       <Hero/>
+      <Fragrance/>
+      <SkinCare/>
+      
+      </div>
+      
     </BrowserRouter>
   );
 }
